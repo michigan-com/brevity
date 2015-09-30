3684af8e3e327918f7ba3ee9648ccf10b1923fce
Eric Bower <neurosnap@gmail.com>
2015-09-30 16:49:03 -0400

    setup basic testing and building frameworks

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@
 
 var gulp = require('gulp');
 var requireDir = require('require-dir');
+var gutil = require('gulp-util');
 
 requireDir('./tasks', { recurse: true });
 
 // Default task
 gulp.task('default', ['sass', 'browserify']);
-
+gulp.task('test', function() {
+  gutil.log('Done, without errors.');
+});
